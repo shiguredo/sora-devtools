52e26de6a097aa853b7a1571bc78c27b2dd523f7
Yuki Ito <yuito@shiguredo.jp>
2020-12-04 11:39:22 +0900

    page に footer を追加する

--- src/pages/multi_recvonly.tsx
+++ src/pages/multi_recvonly.tsx
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 
 import ColDebug from "@/components/ColDebug";
 import ColDemo from "@/components/ColDemo";
+import Footer from "@/components/Footer";
 import Head from "@/components/Head";
 import Header from "@/components/Header";
 import { disconnectSora, setInitialParameter, setMediaDevices } from "@/slice";
@@ -41,6 +42,7 @@ const MultiRecvonly: React.FC = () => {
           </div>
         </div>
       </main>
+      <Footer />
     </>
   );
 };
--- src/pages/multi_simulcast_sendonly.tsx
+++ src/pages/multi_simulcast_sendonly.tsx
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 
 import ColDebug from "@/components/ColDebug";
 import ColDemo from "@/components/ColDemo";
+import Footer from "@/components/Footer";
 import Head from "@/components/Head";
 import Header from "@/components/Header";
 import { disconnectSora, setInitialParameter, setMediaDevices } from "@/slice";
@@ -61,6 +62,7 @@ const MultiSimulcastSendonly: React.FC = () => {
           </div>
         </div>
       </main>
+      <Footer />
     </>
   );
 };
