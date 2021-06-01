4ae6e7bb100ccad1420d29a12c7d57613f557193
Yuki Ito <yuito@shiguredo.jp>
2021-06-01 16:01:57 +0900

    ログの日付表示を修正する

--- src/utils.ts
+++ src/utils.ts
@@ -180,10 +180,10 @@ export function formatUnixtime(time: number): string {
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
-  const hour = date.getHours();
-  const minute = date.getMinutes();
-  const second = date.getSeconds();
-  const millisecond = date.getMilliseconds();
+  const hour = date.getHours().toString().padStart(2, "0");
+  const minute = date.getMinutes().toString().padStart(2, "0");
+  const second = date.getSeconds().toString().padStart(2, "0");
+  const millisecond = date.getMilliseconds().toString().padStart(3, "0");
   return `${year}-${month}-${day} ${hour}:${minute}:${second}.${millisecond}`;
 }
 
