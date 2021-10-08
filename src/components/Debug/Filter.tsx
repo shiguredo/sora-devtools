11f897a79259126fced7a9e5b5fe476c15f8678c
Yuki Ito <yuito@shiguredo.jp>
2021-10-08 15:15:02 +0900

    filter を timeline のみで使えるようにする

--- src/components/Debug/Filter.tsx
+++ src/components/Debug/Filter.tsx
@@ -6,13 +6,14 @@ import { setDebugFilterText } from "@/app/slice";
 
 export const Filter: React.FC = () => {
   const debugFilterText = useAppSelector((state) => state.debugFilterText);
+  const debugType = useAppSelector((state) => state.debugType);
   const dispatch = useAppDispatch();
   const onChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     dispatch(setDebugFilterText(event.target.value));
   };
   return (
     <FormGroup className="form-inline form-debug-filter" controlId="channelId">
-      <FormControl type="text" placeholder="Filter" value={debugFilterText} onChange={onChange} autoComplete="off" />
+      <FormControl type="text" placeholder="Filter" value={debugFilterText} onChange={onChange} autoComplete="off" disabled={debugType !== "timeline"} />
     </FormGroup>
   );
 };
