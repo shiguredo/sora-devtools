4f210a0205cc6046960b083bbef2e283461763b5
Yuki Ito <yuito@shiguredo.jp>
2022-01-05 14:32:06 +0900

    SWC の設定を off にする

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   compress: true,
+  swcMinify: false,
   generateBuildId: async () => "sora_demo",
   exportPathMap: async (defaultPathMap, _settings) => {
     return defaultPathMap;
