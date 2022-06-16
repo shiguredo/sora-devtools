a78680da35f244e9648589dcc57a7c296cc7d9d1
Yuki Ito <yuito@shiguredo.jp>
2022-06-16 16:38:33 +0900

    ID ミスを修正する

--- src/components/DevtoolsPane/BlurRadiusForm.tsx
+++ src/components/DevtoolsPane/BlurRadiusForm.tsx
@@ -19,7 +19,7 @@ export const BlurRadiusForm: React.FC = () => {
   };
   const disabled = mediaType !== "getUserMedia";
   return (
-    <FormGroup className="form-inline" controlId="spotlightNumber">
+    <FormGroup className="form-inline" controlId="blurRadius">
       <TooltipFormLabel kind="blurRadius">blurRadius:</TooltipFormLabel>
       <FormSelect value={blurRadius} onChange={onChange} disabled={disabled}>
         {BLUR_RADIUS.map((value) => {
