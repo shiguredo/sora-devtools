d5e344755e7c9016a5b3ee57d99c3b3325d52dc1
Takeru Ohta <phjgt308@gmail.com>
2023-01-11 11:31:32 +0900

    Apply formatter

--- src/utils.ts
+++ src/utils.ts
@@ -65,7 +65,7 @@ export function copy2clipboard(text: string): Promise<void> {
 export function checkFormValue<T extends readonly string[]>(
   value: unknown,
   candidates: T
-): value is typeof candidates[number] {
+): value is (typeof candidates)[number] {
   if (typeof value === "string") {
     return candidates.indexOf(value) >= 0;
   }
@@ -92,7 +92,7 @@ export function parseQueryString(): Partial<QueryStringParameters> {
   const parseSpecifiedStringParameter = <T extends readonly string[]>(
     parameter: string | (string | null)[] | null,
     candidates: T
-  ): typeof candidates[number] | undefined => {
+  ): (typeof candidates)[number] | undefined => {
     if (checkFormValue(parameter, candidates)) {
       return parameter;
     }
@@ -230,7 +230,7 @@ export function getValueByAspectRatio(aspectRatio: string): number {
 }
 
 // devtools の blurRadius 文字列に対する数値を返す
-export function getBlurRadiusNumber(blurRadius: typeof BLUR_RADIUS[number]): number {
+export function getBlurRadiusNumber(blurRadius: (typeof BLUR_RADIUS)[number]): number {
   switch (blurRadius) {
     case "weak":
       return 5;
@@ -246,10 +246,10 @@ export function getBlurRadiusNumber(blurRadius: typeof BLUR_RADIUS[number]): num
 // getUserMedia の audio constraints を生成
 type CreateAudioConstraintsParameters = {
   audio: boolean;
-  autoGainControl: typeof AUTO_GAIN_CONTROLS[number];
-  noiseSuppression: typeof NOISE_SUPPRESSIONS[number];
-  echoCancellation: typeof ECHO_CANCELLATIONS[number];
-  echoCancellationType: typeof ECHO_CANCELLATION_TYPES[number];
+  autoGainControl: (typeof AUTO_GAIN_CONTROLS)[number];
+  noiseSuppression: (typeof NOISE_SUPPRESSIONS)[number];
+  echoCancellation: (typeof ECHO_CANCELLATIONS)[number];
+  echoCancellationType: (typeof ECHO_CANCELLATION_TYPES)[number];
   audioInput: string;
 };
 export function createAudioConstraints(parameters: CreateAudioConstraintsParameters): boolean | MediaTrackConstraints {
@@ -503,7 +503,7 @@ export function parseMetadata(enabledMetadata: boolean, metadata: string): Json 
   return metadata;
 }
 
-export function getDefaultVideoCodecType(): typeof VIDEO_CODEC_TYPES[number] {
+export function getDefaultVideoCodecType(): (typeof VIDEO_CODEC_TYPES)[number] {
   // getCapabilities API が存在しない場合
   if (!window.RTCRtpSender || !RTCRtpSender.getCapabilities) {
     return "VP9";
