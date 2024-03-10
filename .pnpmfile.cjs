3f5157689cb39fed44e7c14d8c3adc5e14ba59f2
voluntas <nakai@shiguredo.jp>
2024-03-10 12:48:35 +0900

    sora-js-sdk が up -L したときに上がってしまうのを対策する

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -7,4 +7,16 @@ module.exports = {
       return packageJson
     },
   },
+  packageExtensions: {
+    'sora-js-sdk': {
+      peerDependencyMeta: {
+        'sora-js-sdk': {
+          optional: true,
+        },
+      },
+      dependencies: {
+        'sora-js-sdk': ({ version }) => (version.includes('canary') ? 'canary' : 'latest'),
+      },
+    },
+  },
 }
