c3f4f3da8576cafd1257cdecb9c24f4920b6c899
voluntas <nakai@shiguredo.jp>
2024-03-11 20:16:13 +0900

    vitest の実行先を include で絞る

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,15 @@
-import react from '@vitejs/plugin-react';
-import { UserConfigExport, defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react'
+import { type UserConfigExport, defineConfig } from 'vitest/config'
 
 // https://vitejs.dev/config/
 const config: UserConfigExport = {
   plugins: [react()],
   test: {
+    // 一旦 app.spec.ts だけをテストするようにする
+    include: ['src/app/app.spec.ts'],
     globals: true,
     environment: 'jsdom',
   },
-};
+}
 
-export default defineConfig(config);
+export default defineConfig(config)
