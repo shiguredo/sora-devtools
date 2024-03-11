e38f79ef5bef6ee43d283da4b2fefaa384310b2f
voluntas <nakai@shiguredo.jp>
2024-03-11 20:34:50 +0900

    qs を追加

--- tests/simple.test.ts
+++ tests/simple.test.ts
@@ -1,5 +1,12 @@
 import { test } from '@playwright/test'
 
 test('simple', async ({ page }) => {
-  await page.goto('http://localhost:3333/')
+  // multistream=true&role=sendrecv&videoCodecType=VP9
+  const params = new URLSearchParams({
+    multistream: 'true',
+    role: 'sendrecv',
+    videoCodecType: 'VP9',
+  })
+
+  await page.goto(`http://localhost:3333/devtools/?${params.toString()}`)
 })
