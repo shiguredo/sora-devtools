09ca73f5f976762bc0b48d14abce3151d807c316
Takeru Ohta <phjgt308@gmail.com>
2024-10-28 14:19:23 +0900

    MP4 ファイルのロードに失敗した場合のハンドリングをちゃんとする

--- src/components/DevtoolsPane/Mp4FileForm.tsx
+++ src/components/DevtoolsPane/Mp4FileForm.tsx
@@ -20,8 +20,19 @@ export const Mp4FileForm: React.FC = () => {
     if (files === null || files.length === 0) {
       return
     }
-    const mp4MediaStream = await Mp4MediaStream.load(files[0])
-    dispatch(setMp4MediaStream(mp4MediaStream))
+
+    // 以前の内容が残っていた場合に備えて事前に null を入れておく
+    dispatch(setMp4MediaStream(null))
+
+    // MP4 ファイルをロードする
+    try {
+      const mp4MediaStream = await Mp4MediaStream.load(files[0])
+      dispatch(setMp4MediaStream(mp4MediaStream))
+    } catch (e) {
+      // ロードに失敗したらファイル選択をクリアする
+      event.target.value = ""
+      throw e
+    }
   }
   if (mediaType !== 'mp4Media') {
     return null
