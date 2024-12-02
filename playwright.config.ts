626b8cb04259254756b0c20b544f10372488c759
voluntas <nakai@shiguredo.jp>
2024-12-02 10:36:16 +0900

    port 番号は 3333 を継続する

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,7 +33,6 @@ export default defineConfig({
   ],
   webServer: {
     // vite で起動している
-    // TODO: ポート指定方法を後で調べる
     command: 'pnpm run dev',
     url: 'http://localhost:3333/',
     reuseExistingServer: !process.env.CI,
